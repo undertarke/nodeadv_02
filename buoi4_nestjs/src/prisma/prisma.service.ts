358c4b3dec83a98172a9f835efc42606a03bb9b4
undertarke <sangrom2003@gmail.com>
2024-10-23 13:38:38 +0700

    .

--- buoi4_nestjs/src/prisma/prisma.service.ts
+++ buoi4_nestjs/src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client-postgres';
-import { PrismaClient as PrismaClientMysql } from '@prisma/client-mysql';
+import { PrismaClient } from '.prismas/client-postgres';
+import { PrismaClient as PrismaClientMysql } from '.prismas/client-mysql';
 @Injectable()
 export class PrismaPostgreService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
